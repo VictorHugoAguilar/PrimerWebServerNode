bf888d75cb3f8f2b8c527608e44b51b969d01bac
victorHugoAguilar <victorhugoaguilar@aol.com>
2019-10-11 22:13:50 +0200

    creando nuestro primer server con express

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const color = require('colors');
+
+// creando un middleware
+app.use(express.static(__dirname + '/public'))
+
+/*
+app.get('/', (req, res) => {
+
+    let salida = {
+        nombre: 'Victor',
+        edad: 34,
+        url: req.url
+    }
+
+    res.send(salida)
+        // res.send('<html> <body> <h1> Hola Victor </h1></body> </html>')
+
+    // res.send('Hola mundo desde servidor express')
+})
+*/
+
+app.listen(3000, () => {
+    console.log('Escuchando peticiones por el puerto 3000'.bgCyan)
+});
